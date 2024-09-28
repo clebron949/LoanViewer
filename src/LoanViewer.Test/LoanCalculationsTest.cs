5f2a35a201777c8eb4bea49248ab6b2a1c08d1a3
clebron949 <christopher.lebron1@gmail.com>
2024-09-28 14:55:56 -0400

    Add Test project

---
+++ src/LoanViewer.Test/LoanCalculationsTest.cs
@@ -0,0 +1,15 @@
+namespace LoanViewer.Test;
+
+public class LoanCalculationsTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void FirstTest()
+    {
+        Assert.Pass();
+    }
+}
